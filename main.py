0d6e8ab171b3e0af4d031db8e7640481e73dc9f7
ioakeim-h <ioakeim.h@gmail.com>
2025-06-20 14:46:48 +0300

    Finish project

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
-from stats import count_words, count_chars
+import sys
+from stats import count_words, count_characters, sort_characters_dict
 
 def main():
-    filepath = "./books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
     contents = get_book_text(filepath)
 
     n_words = count_words(contents)
-    print(f"{n_words} words found in the document")
+    n_chars_dict = count_characters(contents)
 
-    char_frequency = count_chars(contents)
-    print(char_frequency)
+    sorted_list_of_dicts = sort_characters_dict(n_chars_dict)
+    print_report(n_words, sorted_list_of_dicts, filepath)
 
 def get_book_text(filepath):
     with open(filepath) as f:
         contents = f.read()
     return contents
 
+def print_report(n_words, list_of_dicts, filepath):
+    print(f"""
+============ BOOKBOT ============
+Analyzing book found at {filepath}
+----------- Word Count ----------
+Found {n_words} total words
+--------- Character Count -------
+""", end=""
+    )
 
+    for d in list_of_dicts:
+        if d["char"].isalpha():
+            print(f"{d['char']}: {d['num']}")
+    print("============= END ===============")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -3,5 +3,18 @@ from collections import Counter
 def count_words(text):
     return len(text.split())
 
-def count_chars(text):
-    return Counter(text.lower())
+def count_characters(text):
+    return dict(Counter(text.lower()))
+
+def sort_characters_dict(n_chars_dict):
+    def sort_on(items):
+        return items["num"]
+    
+    list_of_dicts = [{"char": k, "num": v} for k, v in n_chars_dict.items()]
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
+
+
+
+
+
